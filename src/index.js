b7582005d598495545b362227fa3bcea80ba0b6e
AnLeonovich <novka666@mail.ru>
2018-02-25 15:49:16 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    var arr = [];
+    var zeros = 0;
+  
+    for (var i = 0; i < 11; i++){ //т.к number <= 10^8
+        arr[i] = Math.pow(5, i+1);
+    }
+  
+    for(i = 0; i < arr.length; i++ ){
+        zeros += Math.floor(number/arr[i]);
+    }
+
+  return zeros;
 }
